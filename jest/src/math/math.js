50a64e1dbbab1a2da1b3cde3195a5750f0625060
Sergey Koshelnikov <koshelnikovsa@gmail.com>
2023-01-19 16:56:59 +0400

    Removed code

--- jest/src/math/math.js
+++ jest/src/math/math.js
@@ -1,13 +1,3 @@
 const factorial = (x) => {
-    let result = 1;
-    if (x === 1) {
-
-    } else {
-
-    }
-    // for (let i = 1; i <= x; ++i) {
-    //   result *= i;
-    // }
-    return result;
 };
 module.exports = { factorial };
